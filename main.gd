cc157dfe43f43f76c4d29a538abf2c36203ea8a0
mchen2458 <melissa.chen.159@gmail.com>
2025-10-15 12:33:24 -0700

    created exit game functionality

--- main.gd
+++ main.gd
@@ -1,11 +1,15 @@
 extends Node
 
+@onready var exitMenuButton = $OpenExitMenu as Button
 @onready var buyButton = $Menu/BuyFrame/Buy as Button
 @onready var collectionButton = $Menu/CollectionFrame/Collection as Button
 @onready var testButton = $TestingButton as Button
 
 func _ready():
 	handle_connecting_signals()
+	
+func onExitMenuPressed() -> void:
+	get_tree().change_scene_to_file("res://exit_menu.tscn")
 	
 func onBuyPressed() -> void:
 	get_tree().change_scene_to_file("res://buy_page.tscn")
@@ -18,6 +22,7 @@ func onTestPressed() -> void:
 	testButton.set_text(rand)
 	
 func handle_connecting_signals():
+	exitMenuButton.button_down.connect(onExitMenuPressed)
 	buyButton.button_down.connect(onBuyPressed)
 	collectionButton.button_down.connect(onCollectionPressed)
 	testButton.button_down.connect(onTestPressed)
