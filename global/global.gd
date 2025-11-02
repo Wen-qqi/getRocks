b572f38503c8a9ca56751e6e655c478011d8a534
Wen Fan <wqfan05@gmail.com>
2025-11-02 15:06:54 -0800

    added commets and fixed the coin to change to the plusButton texture when clicked

--- global/global.gd
+++ global/global.gd
@@ -21,27 +21,39 @@ var shelf_rocks := {"shelf1": null,
 
 var current_shelf = ""
 
-func get_rarity_price(rarity: String) -> int:
+var plusButton = {'image' = "res://art/plusButton.png"}
+
+func get_rarity_price(rarity: String) -> int: 
+	# Checks rarity of the rocks and return the price depending on the rarity
 	if rarity == "rare":
 		return coin["rare"]
 	return coin["common"]
 
 func start_selling_timer(shelf_name: String, rarity: String) -> void:
-	var timer := Timer.new()
-	timer.one_shot = true
-	if rarity == "rare":
-		timer.wait_time = randf_range(10.0, 20.0)
+	# Starts a timer after a rock has been displayed
+	var timer := Timer.new() # Create a timer
+	timer.one_shot = true # Timer only runs once and will stop after reaching the end
+	# Run a different timer depending on the rarity
+	if rarity == "rare": 
+		timer.wait_time = randf_range(10.0, 20.0) # 10-20 seconds
 	else:
-		timer.wait_time = randf_range(5.0, 10.0)
-	add_child(timer)
-	timer.connect("timeout", Callable(self,"on_rock_sold").bind(shelf_name))
-	timer.start()
+		timer.wait_time = randf_range(5.0, 10.0) # 5-10 second
+	add_child(timer) # Add a child node timer
+	
+	# Links to the signal "timeout" and will call on_rock_sold
+	# It will pass shelf_name as an argument to on_rock_sold
+	# When the timer runs out, it will call on_rock_sold(shelf_name)
+	timer.connect("timeout", Callable(self,"on_rock_sold").bind(shelf_name)) 
+	timer.start() # Start timer
 
 func on_rock_sold(shelf_name: String) -> void:
-	if shelf_rocks.has(shelf_name):
+	if shelf_rocks.has(shelf_name): # Checks if shelf_rocks has the name of the shelf that was inputted
 		var data = shelf_rocks[shelf_name]
-		if data != null and typeof(data) == TYPE_DICTIONARY and data["state"] == "displaying":
+		# If the state of data is displaying and not null(empty), it will display the coin image and set the state to sold
+		if data != null and typeof(data) == TYPE_DICTIONARY and data["state"] == "displaying": 
 			data["texture"] = load(coin["image"])
 			data["state"] = "sold"
+			# Update the data for the select shelf
 			shelf_rocks[shelf_name] = data
+			# Emits signal that the rock is sold for that shelf
 			emit_signal("shelf_sold", shelf_name)
