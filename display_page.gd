717a16c550dd131ee34bb4c2ebbcd65afb3985f9
Wen Fan <wqfan05@gmail.com>
2025-11-05 11:37:00 -0800

    Reapply "added commets and fixed the coin to change to the plusButton texture when clicked"
    This reverts commit 28180a0be33919927e3d0f03e5ab77c65322cbeb.

--- display_page.gd
+++ display_page.gd
@@ -13,18 +13,23 @@ func on_exit_pressed() -> void:
 
 
 func assign_rock(rock_texture: Texture, rarity: String) -> void:
+	# Check if the player actually selected a shelf
 	if Global.current_shelf != "":
+		# Update the shelf to the rock texture and the state to displaying
 		Global.shelf_rocks[Global.current_shelf] = {
 			"texture": rock_texture,
 			"rarity": rarity,
 			"state": "displaying"
 		}
 	get_tree().change_scene_to_file("res://main.tscn")
+	# Start the timer 
 	Global.start_selling_timer(Global.current_shelf, rarity)
 
 func _on_rock_sold(shelf_name: String) -> void:
+	# Checks if there if the shelf_name is in shelf_rocks
 	if Global.shelf_rocks.has(shelf_name):
 		var shelf_data = Global.shelf_rocks[shelf_name]
+		# If the state is displaying, update the texture to the coin and the state to sold
 		if shelf_data["state"] == "displaying":
 			shelf_data["texture"] = load(Global.coin["image"])
 			shelf_data["state"] = "sold"
