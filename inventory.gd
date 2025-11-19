4e9fc78f984581538292a7f285dbebec90ba09e2
mchen2458 <melissa.chen.159@gmail.com>
2025-11-19 11:59:53 -0800

    actually changed the images

--- inventory.gd
+++ inventory.gd
@@ -1,1 +1,11 @@
 extends Node
+
+var red = {"count": 0, "rarity": "common", "image": "res://art/redGemStone.png"}
+var yellow = {"count": 0, "rarity": "common", "image": "res://art/yellowGemStone.png"}
+var green = {"count": 0, "rarity": "common", "image": "res://art/GreenGemStone.png"}
+var blue = {"count": 0, "rarity": "common", "image": "res://art/blueGemStone4.png"}
+var purple = {"count": 0, "rarity": "common", "image": "res://art/purpleGemStone.png"}
+var sunset = {"count": 0, "rarity": "rare", "image": "res://art/colorfulGemStone1.png"}
+var sherbet = {"count": 0, "rarity": "rare", "image": "res://art/purpleGemStone4.png"}
+
+var silver = {"count": 0, "image": "res://art/silverBar.png"}
